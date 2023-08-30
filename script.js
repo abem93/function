4dfd7fce843c33abe5e5846be3f5e58b4bb3e4ad
Abraham Mejia <aimejia4@gmail.com>
2023-08-30 01:49:06 -0500

    Work completed

---
+++ script.js
@@ -0,0 +1,22 @@
+// Write a minimum of three distinct functions:
+// A no-parameter function displaying a greeting.
+
+function sayHi() {
+  alert('Hello World!');
+};
+sayHi();
+
+// A function accepting a name as its parameter to show a personalized greeting.
+
+function hello(name){
+  console.log('Good morning, ' + name);
+};
+
+hello(abraham);
+// A function taking two numbers as parameters, returning their sum.
+
+function calculateSum(a,b){
+  return (a + b);
+};
+
+calculateSum(5,4);
